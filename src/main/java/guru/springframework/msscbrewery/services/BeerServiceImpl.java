ddc9b672635f4881c537e36ba5b2b0478b7db4df
b.gashi <gashi-blenda@hotmail.com>
2025-01-28 19:02:46 +0100

    Creating DELETE endpoint for Beer

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+@Slf4j
 @Service
+
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -25,6 +27,11 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl
+    }
+
+    @Override
+    public void deleteById(UUID beerId){
+        log.debug("Deleting a beer ....");
     }
 
 }
