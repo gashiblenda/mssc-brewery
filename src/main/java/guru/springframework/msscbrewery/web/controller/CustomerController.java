1c98f6e07c5a91cbd653158c2ab292d8f30f7732
b.gashi <gashi-blenda@hotmail.com>
2025-01-30 00:58:05 +0100

    Creating POST, PUT, DELETE endpoints for Customer

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,4 +24,26 @@ public class CustomerController {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customer){
+
+        CustomerDto savedCustomer = customerService.saveNewCustomer(customer);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", "api/v1/customer"+savedCustomer.getCustomerId().toString());
+
+        return new ResponseEntity(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+
+    public void updateCustomer(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customer){
+        customerService.updateCustomer(customerId, customer);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId){
+        customerService.deleteById(customerId);
+    }
 }
